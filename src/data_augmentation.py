4fa960898ce7dd782f3a70a1bff03242576c3ad1
Nima Adibpour <nima.adibpour@mail.mcgill.ca>
2018-03-14 10:53:06 -0400

    Update generator for thread safety

--- src/data_augmentation.py
+++ src/data_augmentation.py
@@ -2,8 +2,50 @@ from keras.datasets import mnist
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.preprocessing.image import ImageDataGenerator
+import threading
+import itertools
 
 
+def grouper(n, iterable):
+    '''
+    Given an iterable, it'll return size n chunks per iteration.
+    Handles the last chunk too.
+    '''
+    it = iter(iterable)
+    while True:
+        chunk = tuple(itertools.islice(it, n))
+        if not chunk:
+            return
+        yield chunk
+
+
+class threadsafe_iter:
+    """
+    Takes an iterator/generator and makes it thread-safe by
+    serializing call to the `next` method of given iterator/generator.
+    """
+    def __init__(self, it):
+        self.it = it
+        self.lock = threading.Lock()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        with self.lock:
+            return self.it.__next__()
+
+
+def threadsafe_generator(f):
+    """
+    A decorator that takes a generator function and makes it thread-safe.
+    """
+    def g(*a, **kw):
+        return threadsafe_iter(f(*a, **kw))
+    return g
+
+
+@threadsafe_generator
 def get_mnist_image_generator(X_train):
     width_shift = 0.2
     height_shift = 0.2
